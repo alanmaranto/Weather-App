eca1c1dea62d1a70be325eb0b2cc94ce000d94b0
alan <alanarostegui@hotmail.com>
2020-07-29 20:51:26 -0500

    feature(header): add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Header from './components/Header';
 import './index.css';
 
 function App() {
   return (
-    <h1>Weather</h1>
+    <>
+    <Header title='Weather React App' /> 
+    </>
   );
 }
 
